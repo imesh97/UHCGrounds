e2a21b5e67aecfb98d4465ffaabebe1d778cc627
imesh97 <imesh.nimsitha@gmail.com>
2016-05-31 17:14:09 -0400

    Added the FileManager and Debugger classes.

---
+++ src/main/java/xyz/imdafatboss/uhcgrounds/utils/Debugger.java
@@ -0,0 +1,13 @@
+package xyz.imdafatboss.uhcgrounds.utils;
+
+public class Debugger {
+
+    public static void debug(String message) {
+        System.out.println("[DEBUGGER: " + getStackTraceElement().getFileName() + " " + getStackTraceElement().getLineNumber() + "] " + message);
+    }
+
+    private static StackTraceElement getStackTraceElement() {
+        return Thread.currentThread().getStackTrace()[3];
+    }
+
+}

64bbe08c2147020de9fee74a438349e2f21bd16e
imesh97 <imesh.nimsitha@gmail.com>
2016-06-01 23:03:03 -0400

    Updated Home class to save default configs, and register the events.

--- src/main/java/xyz/imdafatboss/uhcgrounds/Home.java
+++ src/main/java/xyz/imdafatboss/uhcgrounds/Home.java
@@ -2,19 +2,29 @@ package xyz.imdafatboss.uhcgrounds;
 
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.imdafatboss.uhcgrounds.config.FileManager;
+import xyz.imdafatboss.uhcgrounds.events.Events;
 import xyz.imdafatboss.uhcgrounds.kits.KitManager;
 
 public class Home extends JavaPlugin implements Listener{
 
+    FileManager fm = new FileManager(this);
     KitManager km;
+    Events evt;
 
     @Override
     public void onEnable(){
 
         this.getLogger().info("Created by imdafatboss");
         km = new KitManager(this);
+        evt = new Events(this);
+
+        fm.getConfig("config.yml").saveDefaultConfig();
+        fm.getConfig("data.yml").saveDefaultConfig();
+        fm.getConfig("kit.yml").saveDefaultConfig();
 
         km.updateKit();
+        evt.registerEvents(this);
 
     }
 

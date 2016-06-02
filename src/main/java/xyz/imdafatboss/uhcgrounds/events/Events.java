c64aa6c42bdb3d7d52f1ecbe07b1a26720042808
imesh97 <imesh.nimsitha@gmail.com>
2016-06-01 23:02:31 -0400

    Event manager to register events.

---
+++ src/main/java/xyz/imdafatboss/uhcgrounds/events/Events.java
@@ -0,0 +1,24 @@
+package xyz.imdafatboss.uhcgrounds.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import xyz.imdafatboss.uhcgrounds.Home;
+
+public class Events {
+
+    Home plugin;
+    public Events(Home instance){
+
+        this.plugin = instance;
+
+    }
+
+    public void registerEvents(Home jp){
+
+        PluginManager pm = Bukkit.getServer().getPluginManager();
+
+        pm.registerEvents(new GoldenHeadEvents(jp), plugin);
+
+    }
+
+}

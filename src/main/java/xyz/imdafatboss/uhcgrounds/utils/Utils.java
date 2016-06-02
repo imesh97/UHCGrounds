a784d7007a93b3af88a8602b6022b015659a1636
imesh97 <imesh.nimsitha@gmail.com>
2016-06-01 23:20:15 -0400

    New method to get time left in a String.

--- src/main/java/xyz/imdafatboss/uhcgrounds/utils/Utils.java
+++ src/main/java/xyz/imdafatboss/uhcgrounds/utils/Utils.java
@@ -43,4 +43,21 @@ public class Utils {
 
     }
 
+    public static String getTimeLeft(long time)
+    {
+        long left = time - System.currentTimeMillis();
+        int seconds = (int)(left / 1000L) % 60;
+        int minutes = (int)(left / 60000L % 60L);
+        int hours = (int)(left / 3600000L % 24L);
+        int days = (int)(left / 86400000L);
+        if (days > 1)
+            return days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+        if (hours > 1)
+            return hours + "h " + minutes + "m " + seconds + "s";
+        if (minutes > 1) {
+            return minutes + "m " + seconds + "s";
+        }
+        return seconds + "s";
+    }
+
 }

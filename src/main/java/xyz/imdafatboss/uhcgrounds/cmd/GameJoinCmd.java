e8d72cc865acafc5b46ca0f95c3587c8d1d11d35
imesh97 <imesh.nimsitha@gmail.com>
2016-06-04 19:56:49 -0400

    Command to let players join a game.

---
+++ src/main/java/xyz/imdafatboss/uhcgrounds/cmd/GameJoinCmd.java
@@ -0,0 +1,92 @@
+package xyz.imdafatboss.uhcgrounds.cmd;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import xyz.imdafatboss.uhcgrounds.Home;
+import xyz.imdafatboss.uhcgrounds.cmd.api.CommandFactory;
+import xyz.imdafatboss.uhcgrounds.config.ConfigYML;
+import xyz.imdafatboss.uhcgrounds.config.Messages;
+import xyz.imdafatboss.uhcgrounds.game.*;
+import xyz.imdafatboss.uhcgrounds.player.*;
+
+public class GameJoinCmd extends CommandFactory{
+
+    Home plugin;
+    JavaPlugin jp;
+    public GameJoinCmd(JavaPlugin instance){
+
+        super("uhgjoin", false);
+        this.jp = instance;
+
+    }
+    Messages msg;
+    ConfigYML cfg;
+    Lobby lobby;
+
+    public void execute(CommandSender sender, String[] args){
+
+        msg = new Messages(plugin);
+        cfg = new ConfigYML(plugin);
+        lobby = new Lobby(plugin);
+        Player p = (Player) sender;
+        if(PlayerManager.isUHCPlayer(p)){
+
+            UHCPlayer player = PlayerManager.getPlayer(p);
+            if(player.isInGame()){
+
+                p.sendMessage(msg.getPrefix() + msg.getAlreadyInGame());
+                return;
+
+            }
+
+            for(Game game : GameManager.getGames()){
+
+                if(game.isOn()){
+
+                    continue;
+
+                }
+
+                if(!game.isOn()){
+
+                    if(game.isLobby()){
+
+                        if(game.getSize() >= cfg.getMaxPlayers()){
+
+                            if(game.overPlayer(player)){
+
+                                game.addPlayer(player);
+                                p.sendMessage(msg.prefix() + msg.getJoinedLobby());
+                                p.teleport(lobby.getSpawn());
+
+                            }
+
+                            else{
+
+                                continue;
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        else{
+
+            UHCPlayer player = new UHCPlayer(p);
+            PlayerManager.addPlayer(player);
+            p.chat("uhgjoin");
+            return;
+
+        }
+
+    }
+
+}
---
+++ src/main/java/xyz/imdafatboss/uhcgrounds/config/ConfigYML.java
@@ -0,0 +1,35 @@
+package xyz.imdafatboss.uhcgrounds.config;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import xyz.imdafatboss.uhcgrounds.Home;
+
+public class ConfigYML {
+
+    Home plugin;
+    public ConfigYML(Home instance){
+
+        this.plugin = instance;
+
+    }
+    FileManager fm;
+
+    public YamlConfiguration getConfig(){
+
+        fm = new FileManager(plugin);
+        return fm.getConfig("config.yml").get();
+
+    }
+
+    public int getMaxPlayers(){
+
+        return this.getConfig().getInt("max-players");
+
+    }
+
+    public int getMinPlayers(){
+
+        return this.getConfig().getInt("min-players");
+
+    }
+
+}

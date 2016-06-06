3df1cecd100657a3f32044f209044c05bfdec89d
imesh97 <imesh.nimsitha@gmail.com>
2016-06-05 22:53:00 -0400

    Just a spawn class to save the spawn's location.

---
+++ src/main/java/xyz/imdafatboss/uhcgrounds/game/Spawn.java
@@ -0,0 +1,48 @@
+package xyz.imdafatboss.uhcgrounds.game;
+
+import org.bukkit.Location;
+import xyz.imdafatboss.uhcgrounds.Home;
+import xyz.imdafatboss.uhcgrounds.config.FileManager;
+import xyz.imdafatboss.uhcgrounds.utils.Locations;
+
+public class Spawn {
+
+    Home plugin;
+    public Spawn(Home instance){
+
+        this.plugin = instance;
+
+    }
+    FileManager fm;
+
+    private Location spawn;
+
+    public Location getSpawn(){
+
+        return this.spawn;
+
+    }
+
+    public void setSpawn(Location loc){
+
+        this.spawn = loc;
+
+    }
+
+    public void loadData(){
+
+        fm = new FileManager(plugin);
+
+        this.spawn = Locations.stringToLocation(fm.getConfig("config.yml").get().getString("spawn-loc"));
+
+    }
+
+    public void saveData(){
+
+        fm = new FileManager(plugin);
+
+        fm.getConfig("config.yml").get().set("spawn-loc", Locations.locationToString(this.spawn));
+
+    }
+
+}

e95c053cb0f0ab6d25cb565ab8363ef3986266a7
imesh97 <imesh.nimsitha@gmail.com>
2016-06-06 22:12:06 -0400

    Added a new listener for when a player leaves the server.

--- src/main/java/xyz/imdafatboss/uhcgrounds/events/PlayerLeaveGameEvent.java
+++ src/main/java/xyz/imdafatboss/uhcgrounds/events/PlayerLeaveGameEvent.java
@@ -1,9 +1,11 @@
 package xyz.imdafatboss.uhcgrounds.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import xyz.imdafatboss.uhcgrounds.Home;
 import xyz.imdafatboss.uhcgrounds.config.Messages;
 import xyz.imdafatboss.uhcgrounds.game.Spawn;
@@ -40,21 +42,77 @@ public class PlayerLeaveGameEvent implements Listener{
 
                     player.teleport(spawn.getSpawn());
 
-                    for(UHCPlayer p1 : p.getCurrentGame().getPlayers()){
+                    for(final UHCPlayer p1 : p.getCurrentGame().getPlayers()){
 
                         p1.getPlayer().sendMessage(msg.prefix() + msg.getPlayerLeft(p));
+                        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+
+                            @Override
+                            public void run() {
+
+                                int first = p1.getCurrentGame().getFirst();
+                                int now = p1.getCurrentGame().getPlayers().size();
+                                p1.getPlayer().sendMessage(msg.prefix() + msg.getPlayersLeft(first - 1, now));
+
+                            }
+
+                        }, 10L);
 
                     }
 
                     List<UHCPlayer> l = p.getCurrentGame().getPlayers();
                     l.remove(p);
                     p.getCurrentGame().setPlayers(l);
-                    
+
                     p.setInGame(false);
                     p.setCurrentGame(null);
                     p.setDeaths(p.getDeaths() + 1);
 
                 }
+
+            }
+
+        }
+
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e){
+
+        spawn = new Spawn(plugin);
+        msg = new Messages(plugin);
+
+        Player player = e.getPlayer();
+        if(PlayerManager.isUHCPlayer(player)){
+
+            UHCPlayer p = PlayerManager.getPlayer(player);
+            if(p.isInGame()){
+
+                for(final UHCPlayer p1 : p.getCurrentGame().getPlayers()){
+
+                    p1.getPlayer().sendMessage(msg.prefix() + msg.getPlayerLeft(p));
+                    Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+
+                        @Override
+                        public void run() {
+
+                            int first = p1.getCurrentGame().getFirst();
+                            int now = p1.getCurrentGame().getPlayers().size();
+                            p1.getPlayer().sendMessage(msg.prefix() + msg.getPlayersLeft(first - 1, now));
+
+                        }
+
+                    }, 10L);
+
+                }
+
+                List<UHCPlayer> l = p.getCurrentGame().getPlayers();
+                l.remove(p);
+                p.getCurrentGame().setPlayers(l);
+
+                p.setInGame(false);
+                p.setCurrentGame(null);
+                p.setDeaths(p.getDeaths() + 1);
 
             }
 
